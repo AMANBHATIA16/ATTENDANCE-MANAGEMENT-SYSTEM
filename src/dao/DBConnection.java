52f62c2b1255c008ff9ebed157dda366d8dd4cbc
AMANBHATIA16 <amandeep.24scse1011218@galgotiasuniversity.ac.in>
2025-11-25 19:11:45 +0530

    Update DBConnection.java

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -1,1 +1,33 @@
+package dao;
+
+import java.sql.*;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class DBConnection {
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+
+                Properties props = new Properties();
+                FileInputStream fis = new FileInputStream("resources/db.properties");
+                props.load(fis);
+
+                String driver = props.getProperty("db.driver");
+                String url = props.getProperty("db.url");
+                String username = props.getProperty("db.username");
+                String password = props.getProperty("db.password");
+
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, username, password);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
 
