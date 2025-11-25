a91f91675e5357a3daed959ba52ecbc2620fde7c
AMANBHATIA16 <amandeep.24scse1011218@galgotiasuniversity.ac.in>
2025-11-25 19:25:22 +0530

    Update ViewStudentsForm.java

--- src/ui/ViewStudentsForm.java
+++ src/ui/ViewStudentsForm.java
@@ -1,1 +1,36 @@
+package ui;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import model.Student;
+import service.AttendanceService;
+import java.util.List;
+
+public class ViewStudentsForm extends JFrame {
+
+    AttendanceService service = new AttendanceService();
+
+    public ViewStudentsForm() {
+        setTitle("View Students");
+        setSize(500, 400);
+
+        String[] cols = {"ID", "Name", "Course"};
+        DefaultTableModel model = new DefaultTableModel(cols, 0);
+
+        JTable table = new JTable(model);
+        JScrollPane pane = new JScrollPane(table);
+        add(pane);
+
+        List<Student> list = service.getStudents();
+        for (Student s : list) {
+            model.addRow(new Object[]{
+                    s.getStudentId(),
+                    s.getName(),
+                    s.getCourse()
+            });
+        }
+
+        setVisible(true);
+    }
+}
 
