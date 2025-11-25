1018de0dd817351274949e677aee5378828c7ab6
AMANBHATIA16 <amandeep.24scse1011218@galgotiasuniversity.ac.in>
2025-11-25 19:20:26 +0530

    Update Attendance.java

--- src/model/Attendance.java
+++ src/model/Attendance.java
@@ -1,1 +1,20 @@
+package model;
 
+import java.sql.Date;
+
+public class Attendance {
+
+    private int studentId;
+    private Date date;
+    private String status;
+
+    public Attendance(int sid, Date d, String s) {
+        this.studentId = sid;
+        this.date = d;
+        this.status = s;
+    }
+
+    public int getStudentId() { return studentId; }
+    public Date getDate() { return date; }
+    public String getStatus() { return status; }
+}
