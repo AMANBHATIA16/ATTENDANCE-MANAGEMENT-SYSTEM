1e33711df9baaf76d9ad3f24e5d72a7a62f4d265
AMANBHATIA16 <amandeep.24scse1011218@galgotiasuniversity.ac.in>
2025-12-21 00:24:41 +0530

    Update AttendanceServlet.java

--- src/controller/AttendanceServlet.java
+++ src/controller/AttendanceServlet.java
@@ -1,1 +1,31 @@
+package controller;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import service.AttendanceService;
+
+import java.io.IOException;
+
+@WebServlet("/markAttendance")
+public class AttendanceServlet extends HttpServlet {
+
+    private AttendanceService service;
+
+    @Override
+    public void init() {
+        service = new AttendanceService();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        int studentId = Integer.parseInt(req.getParameter("studentId"));
+        String status = req.getParameter("status"); // Present / Absent
+
+        service.markAttendance(studentId, status);
+
+        resp.getWriter().println("Attendance marked successfully");
+    }
+}
